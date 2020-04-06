153d2f63015b0ffb2e498638e118dcd9e04951e7
U370709066 <beratdevecioglu@hotmail.com>
2020-04-06 08:08:42 +0300

    SeniorDesignProject2

---
+++ app/src/main/java/com/example/senior/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.senior;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+    //Variables
+    Animation newAnim;
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Animations
+        newAnim = AnimationUtils.loadAnimation(this,R.anim.new_animation);
+
+        //Hooks
+        image = findViewById(R.id.imageView);
+
+        image.setAnimation(newAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                Pair[] pairs = new Pair [1];
+                pairs[0] = new Pair<View,String>(image, "logo_image");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,options.toBundle());
+            }
+
+        }, SPLASH_SCREEN);
+
+    }
+}
