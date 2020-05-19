5261ae8d61ffab391f43a2180abf2839b4b069fe
U370709066 <beratdevecioglu@hotmail.com>
2020-05-19 06:28:20 +0300

    Updating user data

--- app/src/main/java/com/example/senior/Profile.java
+++ app/src/main/java/com/example/senior/Profile.java
@@ -4,28 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Profile extends AppCompatActivity {
 
     TextView fullNameLabel, emailLabel;
     TextInputLayout  fullname, username, phonenumber, email, password;
+
+    String _NAME, _EMAIL, _PHONENUMBER, _USERNAME, _PASSWORD;
+
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        reference = FirebaseDatabase.getInstance().getReference("accounts");
         //Hooks
         fullNameLabel = findViewById(R.id.fullest_name);
         emailLabel = findViewById(R.id.show_email);
         fullname = findViewById(R.id.fullest_name_profile);
         email = findViewById(R.id.email_profile);
         phonenumber = findViewById(R.id.phonenumber_profile);
-        username = findViewById(R.id.username_profile);
         password = findViewById(R.id.password_profile);
 
         showAllUserData();
@@ -36,22 +45,60 @@ public class Profile extends AppCompatActivity {
 
             Intent intent = getIntent();
 
-            String account_name = intent.getStringExtra("fdname");
-            String account_username = intent.getStringExtra("fdusername");
-            String account_phonenumber = intent.getStringExtra("fdphonenumber");
-            String account_email = intent.getStringExtra("fdemail");
-            String account_password = intent.getStringExtra("fdpassword");
+            _NAME = intent.getStringExtra("fdname");
+            _USERNAME = intent.getStringExtra("fdusername");
+            _PHONENUMBER = intent.getStringExtra("fdphonenumber");
+            _EMAIL = intent.getStringExtra("fdemail");
+            _PASSWORD = intent.getStringExtra("fdpassword");
 
-            fullNameLabel.setText(account_name);
-            emailLabel.setText(account_username);
-            fullname.getEditText().setText(account_name);
-            email.getEditText().setText(account_email);
-            phonenumber.getEditText().setText(account_phonenumber);
-            username.getEditText().setText(account_username);
-            password.getEditText().setText(account_password);
+            fullNameLabel.setText(_NAME);
+            emailLabel.setText(_EMAIL);
+            fullname.getEditText().setText(_NAME);
+            email.getEditText().setText(_EMAIL);
+            phonenumber.getEditText().setText(_PHONENUMBER);
+            password.getEditText().setText(_PASSWORD);
 
 
         }
 
+        public void update(View view) {
 
+            if (isNameChanged() || isPasswordChanged()) {
+                Toast.makeText(this, "Bilgileriniz başarıyla güncellendi.", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(this, "Bilgilerinizin güncellemesi için değişiklik yapmanız gerekiyor.", Toast.LENGTH_LONG).show();
+
+            }
+
+        }
+
+
+
+
+        private boolean isPasswordChanged()  {
+            if(!_PASSWORD.equals(password.getEditText().getText().toString())) {
+                reference.child(_USERNAME).child("fdpassword").setValue(password.getEditText().getText().toString());
+                _PASSWORD = password.getEditText().getText().toString();
+                return true;
+            }else{
+                return false;
+            }
+
+
+         }
+
+       private boolean isNameChanged() {
+
+        if(!_NAME.equals(fullname.getEditText().getText().toString())) {
+            reference.child(_USERNAME).child("fdname").setValue(fullname.getEditText().getText().toString());
+            _NAME = fullname.getEditText().getText().toString();
+            return true;
+        }else{
+            return false;
+        }
+
+
+
+    }
 }
